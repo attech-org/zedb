383d81f9ec29b73c54962b0816793299e1927fb4
Alexandr Tovmach <alexandrtovmach@gmail.com>
2022-05-17 20:17:11 +0200

    feat: Added styling tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import './styles/App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from './redux/store';
 import { changeTheme } from './redux/app/reducers';
+import styled from 'styled-components';
+
+const Layout = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme === "dark" ? 'grey' : 'white'};
+`;
+
+const Button = styled.button`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: black;
+  border: 2px solid black;
+`
 
 const App = () => {
   const dispatch = useDispatch();
@@ -12,10 +32,10 @@ const App = () => {
   }
 
   return (
-    <div className="App">
+    <Layout theme={appTheme}>
       {appTheme}
-      <button onClick={handleThemeClick}>Change Theme</button>
-    </div>
+      <Button onClick={handleThemeClick}>Change Theme</Button>
+    </Layout>
   );
 }
 
